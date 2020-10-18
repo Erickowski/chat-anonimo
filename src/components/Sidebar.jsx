57a4bc9c7d187cbb9858e2e7c037276369ac813b
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-18 15:03:58 -0500

    Filtrando contactos y grupos por nombre

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 
 import ContactoContext from "../context/contactos/contactoContext";
@@ -29,6 +29,35 @@ const Sidebar = () => {
     const chatContext = useContext(ChatContext);
     const { chats, crearChat, abrirChat, activeChat } = chatContext;
 
+    const [filterContacts, saveFilterContacts] = useState("");
+    const [filterGroups, saveFilterGroups] = useState("");
+
+    const [contacts, saveContacts] = useState([...contactos]);
+    const [groups, saveGroups] = useState([...grupos]);
+
+    const handleFilterContacts = (e) => {
+        saveFilterContacts(e.target.value);
+
+        saveContacts(
+            contactos.filter((contacto) =>
+                contacto.nombre
+                    .toLowerCase()
+                    .includes(e.target.value.toLowerCase())
+            )
+        );
+    };
+    const handleFilterGroups = (e) => {
+        saveFilterGroups(e.target.value);
+
+        saveGroups(
+            grupos.filter((grupo) =>
+                grupo.nombre
+                    .toLowerCase()
+                    .includes(e.target.value.toLowerCase())
+            )
+        );
+    };
+
     return (
         <SidebarContainer>
             <h2>Tus chats</h2>
@@ -50,54 +79,72 @@ const Sidebar = () => {
                 </ul>
             )}
             <h2>Tus contactos</h2>
-            {contactos.length === 0 ? (
+            {contacts.length === 0 ? (
                 <p>No tienes contactos, crea uno.</p>
             ) : (
-                <ul>
-                    {contactos.map((contacto) => (
-                        <li key={contacto.id}>
-                            {contacto.nombre}{" "}
-                            {chats.filter((chat) => chat.idType === contacto.id)
-                                .length === 0 && (
-                                <i
-                                    onClick={() =>
-                                        crearChat(
-                                            contacto.nombre,
-                                            contacto.id,
-                                            "person"
-                                        )
-                                    }
-                                    className="fas fa-plus"
-                                ></i>
-                            )}
-                        </li>
-                    ))}
-                </ul>
+                <>
+                    <p>Escribe aqui para buscar contactos por nombre</p>
+                    <input
+                        type="text"
+                        value={filterContacts}
+                        onChange={(e) => handleFilterContacts(e)}
+                    />
+                    <ul>
+                        {contacts.map((contacto) => (
+                            <li key={contacto.id}>
+                                {contacto.nombre}{" "}
+                                {chats.filter(
+                                    (chat) => chat.idType === contacto.id
+                                ).length === 0 && (
+                                    <i
+                                        onClick={() =>
+                                            crearChat(
+                                                contacto.nombre,
+                                                contacto.id,
+                                                "person"
+                                            )
+                                        }
+                                        className="fas fa-plus"
+                                    ></i>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </>
             )}
             <h2>Tus grupos</h2>
-            {grupos.length === 0 ? (
+            {groups.length === 0 ? (
                 <p>No tienes grupos, crea uno.</p>
             ) : (
-                <ul>
-                    {grupos.map((grupo) => (
-                        <li key={grupo.id}>
-                            {grupo.nombre}
-                            {chats.filter((chat) => chat.idType === grupo.id)
-                                .length === 0 && (
-                                <i
-                                    onClick={() =>
-                                        crearChat(
-                                            grupo.nombre,
-                                            grupo.id,
-                                            "group"
-                                        )
-                                    }
-                                    className="fas fa-plus"
-                                ></i>
-                            )}
-                        </li>
-                    ))}
-                </ul>
+                <>
+                    <p>Escribe aqui para buscar grupos por nombre</p>
+                    <input
+                        type="text"
+                        value={filterGroups}
+                        onChange={(e) => handleFilterGroups(e)}
+                    />
+                    <ul>
+                        {groups.map((grupo) => (
+                            <li key={grupo.id}>
+                                {grupo.nombre}
+                                {chats.filter(
+                                    (chat) => chat.idType === grupo.id
+                                ).length === 0 && (
+                                    <i
+                                        onClick={() =>
+                                            crearChat(
+                                                grupo.nombre,
+                                                grupo.id,
+                                                "group"
+                                            )
+                                        }
+                                        className="fas fa-plus"
+                                    ></i>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </>
             )}
         </SidebarContainer>
     );
