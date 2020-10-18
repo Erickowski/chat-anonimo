c491d3bf53dccd9c39dab402f170b4dd819b7398
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-17 22:15:14 -0500

    Mostrar contactos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+
+import ContactoContext from "../context/contactos/contactoContext";
+
+const SidebarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-left: 10px;
+    font-family: "Roboto", sans-serif;
+`;
 
 const Sidebar = () => {
-    return <h1>Sidebar</h1>;
+    const contactoContext = useContext(ContactoContext);
+    const { contactos } = contactoContext;
+
+    return (
+        <SidebarContainer>
+            <h2>Tus contactos</h2>
+            {contactos.length === 0 ? (
+                <p>No tienes contactos, crea uno.</p>
+            ) : (
+                <ul>
+                    {contactos.map((contacto) => (
+                        <li>{contacto.nombre}</li>
+                    ))}
+                </ul>
+            )}
+            <h2>Tus grupos</h2>
+            <p>No tienes grupos, crea uno.</p>
+        </SidebarContainer>
+    );
 };
 
 export default Sidebar;
